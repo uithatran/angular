8181656f39b747984c66a4a1fdcdf38b860923c0
PCHome Ha <hatt15520177@gmail.com>
2020-03-21 10:40:41 +0700

    commit

---
+++ tourof-heroes-app/src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  selectHero: Hero;
+  heroes = HEROES;
+  hero: Hero = {
+    id: 1,
+    name: "Windstorm",
+  }
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSelect(hero1) {
+    this.selectHero = hero1;
+  }
+
+}
